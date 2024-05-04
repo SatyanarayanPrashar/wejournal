a836b5db51f302fc002fd83c9c19d932254b4fc5
Satyanarayan Prashar <satyanarayanprashar73@gmail.com>
2024-05-04 23:19:54 +0530

    stripe x

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -15,10 +15,6 @@ const MarketingPage = () => {
       <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
         <Heading />
         <Heroes />
-        <script async src="https://js.stripe.com/v3/pricing-table.js"></script>
-        <stripe-pricing-table pricing-table-id="prctbl_1PCli6SBXOKMwIx1g61R1FWm"
-          publishable-key="pk_test_51PClCsSBXOKMwIx17TcAAmbDrdqkNYzjSIUK8sZsr4EN1x6jV23rXia8ekbwC3SDUhOrCPznQU6VZ4O5mhO6VS6F00MwUyjHzb">
-        </stripe-pricing-table>
       </div>
       <Footer />
     </div>
